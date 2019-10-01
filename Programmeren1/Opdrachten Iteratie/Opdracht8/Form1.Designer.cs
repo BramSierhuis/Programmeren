74a559210089416867e7169ad301cdb7b11ad1ba
Bram Sierhuis <35657766+BramSierhuis@users.noreply.github.com>
2019-10-01 19:30:32 +0200

    Iteratie 8 af

---
+++ Programmeren1/Opdrachten Iteratie/Opdracht8/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Opdracht8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.lblSom = new System.Windows.Forms.Label();
+            this.lblFormule = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Geef een getal (n)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Som = 1 + 2 + ... + n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Som = n * (n + 1) / 2";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(19, 139);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.TabIndex = 3;
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(16, 44);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(212, 23);
+            this.btnCompare.TabIndex = 4;
+            this.btnCompare.Text = "Vergelijk";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(128, 13);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(100, 20);
+            this.txtN.TabIndex = 5;
+            // 
+            // lblSom
+            // 
+            this.lblSom.AutoSize = true;
+            this.lblSom.Location = new System.Drawing.Point(128, 96);
+            this.lblSom.Name = "lblSom";
+            this.lblSom.Size = new System.Drawing.Size(0, 13);
+            this.lblSom.TabIndex = 6;
+            // 
+            // lblFormule
+            // 
+            this.lblFormule.AutoSize = true;
+            this.lblFormule.Location = new System.Drawing.Point(128, 113);
+            this.lblFormule.Name = "lblFormule";
+            this.lblFormule.Size = new System.Drawing.Size(0, 13);
+            this.lblFormule.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 174);
+            this.Controls.Add(this.lblFormule);
+            this.Controls.Add(this.lblSom);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label lblSom;
+        private System.Windows.Forms.Label lblFormule;
+    }
+}
+
---
+++ Programmeren1/Opdrachten Iteratie/Opdracht8/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Opdracht8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(txtN.Text);
+
+            int som = 0;
+            for(int i = 0; i <= n; i++)
+            {
+                som += i;
+            }
+
+            int formule = n * (n + 1) / 2;
+
+            lblSom.Text = som.ToString();
+            lblFormule.Text = formule.ToString();
+
+            if (som == formule)
+                lblOutput.Text = "De som en formule zijn gelijk";
+            else
+                lblOutput.Text = "De som en formule zijn niet gelijk";
+        }
+    }
+}
