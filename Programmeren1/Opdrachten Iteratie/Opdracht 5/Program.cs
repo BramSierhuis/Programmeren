d62c51b449a3522acfbb2308940e4f7685922eb3
Bram Sierhuis <35657766+BramSierhuis@users.noreply.github.com>
2019-10-01 16:19:20 +0200

    Iteratie 4 en 5 af

---
+++ Programmeren1/Opdrachten Iteratie/Opdracht 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Voer jaartal in: ");
+                int jaartal = int.Parse(Console.ReadLine());
+
+                if (jaartal == 0)
+                    break;
+
+                if (jaartal < 0)
+                    Console.WriteLine("Een jaartal kan geen negatief getal zijn. Probeer opnieuw");
+                else if ((jaartal % 400 == 0 || jaartal % 4 == 0) && jaartal % 100 != 0)
+                    Console.WriteLine(jaartal + " is een schrikkeljaar.");
+                else if(jaartal == 2000)
+                    Console.WriteLine(jaartal + " is bij uitzondering ook een schrikkeljaar.");
+                else
+                    Console.WriteLine(jaartal + " is geen schrikkeljaar.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programmeren1/Opdrachten Iteratie/Opdracht4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 20;
+
+            int x = 1;
+            int y = 1;
+            int z;
+
+            Console.Write("{0}, {1}", x, y);
+
+            for (int i = 2; i < length; i++)
+            {
+                z = x + y;
+                x = y;
+                y = z;
+
+                Console.Write(", " + z);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
