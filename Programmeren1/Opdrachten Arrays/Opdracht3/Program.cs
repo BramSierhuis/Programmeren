62e52d6d7d8f80f24c15e234d963375b90ec4342
Bram SIerhuis <35657766+BramSierhuis@users.noreply.github.com>
2019-10-07 12:50:45 +0200

    Opdrachten tm 4 af

--- Programmeren1/Opdrachten Arrays/Opdracht3/Program.cs
+++ Programmeren1/Opdrachten Arrays/Opdracht3/Program.cs
@@ -13,27 +13,49 @@ namespace Opdracht3
             Console.Write("Geef het aantal studenten: ");
             int size = int.Parse(Console.ReadLine());
 
+            Console.WriteLine();
+
             string[] namen = new string[size];
             double[] cijfers = new double[size];
 
-            for(int i = 0; i < size; i++)
-            {
-                Console.WriteLine("Geef de naam van de {0}e student: ", i);
-                namen[i] = Console.ReadLine();
-            }
+            string topStudent = "";
+            double max = 0;
 
             for (int i = 0; i < size; i++)
             {
-                Console.WriteLine("Geef het cijfer van {0}: ", namen[i]);
-                cijfers[i] = double.Parse(Console.ReadLine());
+                Console.Write("Geef de naam van de {0}e student: ", i + 1);
+                namen[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Geef het cijfer van {0}: ", namen[i]);
+
+                double input = double.Parse(Console.ReadLine());
+                cijfers[i] = input;
+
+                if(input > max)
+                {
+                    max = input;
+                    topStudent = namen[i];
+                }
             }
 
             double avg = cijfers.Sum() / size;
 
             Console.WriteLine();
-            Console.WriteLine("Het gemiddelde cijfer is: " + avg);
-            Console.WriteLine("Het hoogste cijfer is: " + cijfers.Max());
+            Console.WriteLine("Het gemiddelde cijfer is: " + avg.ToString("0.00"));
+            Console.WriteLine("Het hoogste cijfer is: {0} en dit is behaald door {1}", max, topStudent);
+            Console.WriteLine();
+            
+            for(int i = 0; i < namen.Length; i++)
+            {
+                Console.WriteLine("Student {0} heeft voor {1} een {2}", namen[i], name, cijfers[i]);
+            }
 
+            Console.ReadKey();
         }
     }
 }
---
+++ Programmeren1/Opdrachten Arrays/Opdracht4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Opdracht4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 1;
+            int count = 0;
+            int[] numbers = new int[20];
+
+            while (input != 0 && count < 20)
+            {
+                Console.Write("Geef een getal: (0 is exit)");
+                input = int.Parse(Console.ReadLine());
+
+                numbers[count] = input;
+                count++;
+            }
+
+            Console.WriteLine();
+            Console.Write("Geef de zoekwaarde: ");
+            int zoekwaarde = int.Parse(Console.ReadLine());
+            int voorkomen = 0;
+
+            foreach(int number in numbers)
+            {
+                if (number == zoekwaarde)
+                    voorkomen++;
+            }
+
+            Console.WriteLine("De zoekwaarde komt {0} keer voor", voorkomen);
+
+            Console.ReadKey();
+        }
+    }
+}
