f4f150455486e4ad16a32b23d01ffddc79305e4b
Bram Sierhuis <35657766+BramSierhuis@users.noreply.github.com>
2019-10-14 18:54:59 +0200

    Methoden tm 5 af

---
+++ Programmeren1/Opdrachten Methoden/Opdracht5/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Opdracht5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtGetal1 = new System.Windows.Forms.TextBox();
+            this.txtGetal2 = new System.Windows.Forms.TextBox();
+            this.lblUitkomst = new System.Windows.Forms.Label();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnX = new System.Windows.Forms.Button();
+            this.btnDelen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Getal 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Getal 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Uitkomst";
+            // 
+            // txtGetal1
+            // 
+            this.txtGetal1.Location = new System.Drawing.Point(86, 13);
+            this.txtGetal1.Name = "txtGetal1";
+            this.txtGetal1.Size = new System.Drawing.Size(100, 20);
+            this.txtGetal1.TabIndex = 3;
+            // 
+            // txtGetal2
+            // 
+            this.txtGetal2.Location = new System.Drawing.Point(86, 37);
+            this.txtGetal2.Name = "txtGetal2";
+            this.txtGetal2.Size = new System.Drawing.Size(100, 20);
+            this.txtGetal2.TabIndex = 4;
+            // 
+            // lblUitkomst
+            // 
+            this.lblUitkomst.AutoSize = true;
+            this.lblUitkomst.Location = new System.Drawing.Point(83, 139);
+            this.lblUitkomst.Name = "lblUitkomst";
+            this.lblUitkomst.Size = new System.Drawing.Size(35, 13);
+            this.lblUitkomst.TabIndex = 5;
+            this.lblUitkomst.Text = "label4";
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlus.Location = new System.Drawing.Point(16, 80);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(38, 41);
+            this.btnPlus.TabIndex = 6;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseCompatibleTextRendering = true;
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMin
+            // 
+            this.btnMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMin.Location = new System.Drawing.Point(60, 80);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(38, 41);
+            this.btnMin.TabIndex = 7;
+            this.btnMin.Text = "-";
+            this.btnMin.UseCompatibleTextRendering = true;
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // btnX
+            // 
+            this.btnX.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnX.Location = new System.Drawing.Point(104, 80);
+            this.btnX.Name = "btnX";
+            this.btnX.Size = new System.Drawing.Size(38, 41);
+            this.btnX.TabIndex = 8;
+            this.btnX.Text = "x";
+            this.btnX.UseCompatibleTextRendering = true;
+            this.btnX.UseVisualStyleBackColor = true;
+            this.btnX.Click += new System.EventHandler(this.btnX_Click);
+            // 
+            // btnDelen
+            // 
+            this.btnDelen.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelen.Location = new System.Drawing.Point(148, 80);
+            this.btnDelen.Name = "btnDelen";
+            this.btnDelen.Size = new System.Drawing.Size(38, 41);
+            this.btnDelen.TabIndex = 9;
+            this.btnDelen.Text = ":";
+            this.btnDelen.UseCompatibleTextRendering = true;
+            this.btnDelen.UseVisualStyleBackColor = true;
+            this.btnDelen.Click += new System.EventHandler(this.btnDelen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 160);
+            this.Controls.Add(this.btnDelen);
+            this.Controls.Add(this.btnX);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.lblUitkomst);
+            this.Controls.Add(this.txtGetal2);
+            this.Controls.Add(this.txtGetal1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Opdracht 5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtGetal1;
+        private System.Windows.Forms.TextBox txtGetal2;
+        private System.Windows.Forms.Label lblUitkomst;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnX;
+        private System.Windows.Forms.Button btnDelen;
+    }
+}
+
---
+++ Programmeren1/Opdrachten Methoden/Opdracht5/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Opdracht5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            int getal1 = int.Parse(txtGetal1.Text);
+            int getal2 = int.Parse(txtGetal2.Text);
+            double uitkomst = Add(getal1, getal2);
+
+            lblUitkomst.Text = uitkomst.ToString();
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            int getal1 = int.Parse(txtGetal1.Text);
+            int getal2 = int.Parse(txtGetal2.Text);
+            double uitkomst = Substract(getal1, getal2);
+
+            lblUitkomst.Text = uitkomst.ToString();
+        }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            int getal1 = int.Parse(txtGetal1.Text);
+            int getal2 = int.Parse(txtGetal2.Text);
+            double uitkomst = Multiply(getal1, getal2);
+
+            lblUitkomst.Text = uitkomst.ToString();
+        }
+
+        private void btnDelen_Click(object sender, EventArgs e)
+        {
+            int getal1 = int.Parse(txtGetal1.Text);
+            int getal2 = int.Parse(txtGetal2.Text);
+            double uitkomst = Devide(getal1, getal2);
+
+            lblUitkomst.Text = uitkomst.ToString();
+        }
+
+        double Add(int x, int y)
+        {
+            double output = x + y;
+            return output;
+        }
+
+        double Substract(int x, int y)
+        {
+            double output = x - y;
+            return output;
+        }
+
+        double Multiply(int x, int y)
+        {
+            double output = x * y;
+            return output;
+        }
+
+        double Devide(int x, int y)
+        {
+            double output = (double) x / (double) y;
+            return output;
+        }
+    }
+}
