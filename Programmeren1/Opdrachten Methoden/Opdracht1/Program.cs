2041120073d11fdaf82e60a783ddafbf10fcebe4
Bram Sierhuis <35657766+BramSierhuis@users.noreply.github.com>
2019-10-14 17:30:24 +0200

    Opdracht 1 van Methoden af

---
+++ Programmeren1/Opdrachten Methoden/Opdracht1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Opdracht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a price: ");
+            float price = float.Parse(Console.ReadLine());
+            float btw = CalculateBTW(price);
+            float total = btw + price;
+
+            Console.WriteLine("Price: {0}, VAT: {1}, Total: {2}", price, btw.ToString("0.00"), total.ToString("0.00"));
+        }
+
+        static float CalculateBTW(float price)
+        {
+            float btw = price * 0.21f;
+            return btw;
+        }
+    }
+}
