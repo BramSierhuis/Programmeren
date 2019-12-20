644317aa53a9280ab29b6325a6e4005356063a45
Bram SIerhuis <35657766+BramSierhuis@users.noreply.github.com>
2019-12-20 10:24:43 +0100

    Finished week5 - Opdracht 1

---
+++ Programmeren2/Week5/MyTools/LeesTools.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyTools
+{
+    public class LeesTools
+    {
+        public static int LeesInt(string vraag)
+        {
+            Console.Write(vraag);
+            int input = int.Parse(Console.ReadLine());
+
+            return input;
+        }
+
+        public static string LeesString(string vraag)
+        {
+            Console.Write(vraag);
+            string input = Console.ReadLine();
+
+            return input;
+        }
+
+        public static int LeesInt(string vraag, int min, int max)
+        {
+            Console.Write(vraag);
+            int input = int.Parse(Console.ReadLine());
+
+            while (input < min || input > max)
+            {
+                Console.Write(vraag);
+                input = int.Parse(Console.ReadLine());
+            }
+
+            return input;
+        }
+    }
+}
