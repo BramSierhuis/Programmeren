c94192892186186ec8b6e0c73ed4d743f533d1e8
Bram Sierhuis <35657766+BramSierhuis@users.noreply.github.com>
2020-02-11 11:28:46 +0100

    Finished Week1 - Opdracht2

---
+++ Programmeren3/Week1/Opdracht2 -Yahtzee game/Dobbelsteen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht3___Classes
+{
+    class Dobbelsteen
+    {
+        public int waarde;
+        Random rnd;
+
+        public Dobbelsteen(Random rnd)
+        {
+            this.rnd = rnd;
+        }
+
+        public void Gooi()
+        {
+            waarde = rnd.Next(1, 7);
+        }
+
+        public void ToonWaarde()
+        {
+            Console.Write(waarde);
+        }
+    }
+}
---
+++ Programmeren3/Week1/Opdracht2 -Yahtzee game/YahtzeeGame.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht3___Classes
+{
+    class YahtzeeGame
+    {
+        Dobbelsteen[] dobbelstenen = new Dobbelsteen[5];
+
+        public YahtzeeGame()
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < dobbelstenen.Length; i++)
+            {
+                dobbelstenen[i] = new Dobbelsteen(rnd);
+            }
+        }
+
+        public void Gooi()
+        {
+            for (int i = 0; i < dobbelstenen.Length; i++)
+            {
+                dobbelstenen[i].Gooi();
+            }
+        }
+
+        public void ToonWorp()
+        {
+            for (int i = 0; i < dobbelstenen.Length; i++)
+            {
+                dobbelstenen[i].ToonWaarde();
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public bool Yahtzee()
+        {
+            int waarde = dobbelstenen[0].waarde;
+
+            foreach(Dobbelsteen dobbelsteen in dobbelstenen)
+            {
+                if (waarde != dobbelsteen.waarde)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
