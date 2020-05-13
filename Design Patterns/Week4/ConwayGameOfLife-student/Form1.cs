2544a70c3c8295a72530dbf5f24eb8040306c839
Bram SIerhuis <35657766+BramSierhuis@users.noreply.github.com>
2020-05-13 12:48:03 +0200

    Finished Week4 - Opdracht 2

---
+++ Design Patterns/Week4/ConwayGameOfLife-student/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ConwayGameOfLife
+{
+  public partial class Form1 : Form
+  {
+    private ConwayGameOfLife game;
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      game = new ConwayGameOfLife();
+      game.LifeBehaviour = new StandardLife();
+
+      this.DoubleBuffered = true;
+
+      timer1.Enabled = true;
+    }
+
+    private void Form1_Paint(object sender, PaintEventArgs e)
+    {
+      game.Draw(e.Graphics);
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      game.Evolve();
+      this.Refresh();
+    }
+
+    private void btnReset_Click(object sender, EventArgs e)
+    {
+      this.game.Reset();
+    }
+
+        private void btnStandardLife_Click(object sender, EventArgs e)
+        {
+            game.Reset();
+            game.LifeBehaviour = new StandardLife();
+        }
+
+        private void btnHighLife_Click(object sender, EventArgs e)
+        {
+            game.Reset();
+            game.LifeBehaviour = new HighLife();
+        }
+    }
+}
---
+++ Design Patterns/Week4/ConwayGameOfLife-student/HighLife.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwayGameOfLife
+{
+    public class HighLife : ILifeBehaviour
+    {
+        public bool CellShouldLive(bool livingCell, int neighbourCount)
+        {
+            // with 3 or 6 neighbour cell will live
+            if (neighbourCount == 3 || neighbourCount == 6)
+                return true;
+
+            // live cell with 2 or 3 neighbours stays alive
+            if (livingCell && neighbourCount == 2) //Je hoeft niet voor 3 te checken want dat gebeurd hierboven al, en als dat gebeurd wordt er al true gereturned
+                return true;
+
+            // dead cell
+            return false;
+        }
+    }
+}
