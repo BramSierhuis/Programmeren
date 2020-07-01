47a8f2420437c930fcc0a345b9593d2392908eea
Bram SIerhuis <35657766+BramSierhuis@users.noreply.github.com>
2020-07-01 09:35:11 +0200

    Finished Opdracht 4

---
+++ Design Patterns/DesignPatterns-tentamen/Opdracht3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Opdracht3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+
+            Console.ReadKey();
+        }
+
+        private void Start()
+        {
+            StemSysteem stemSysteem1 = StemSysteem.GetInstance();
+            stemSysteem1.Stem("Bram", true);
+            stemSysteem1.Stem("Jake", true);
+            stemSysteem1.Stem("Klaas", false);
+
+            StemSysteem stemSysteem2 = StemSysteem.GetInstance();
+            stemSysteem1.Stem("Mark", false);
+
+            stemSysteem1.Stem("Jake", true);
+            stemSysteem1.Stem("Klaas", false);
+        }
+    }
+}
---
+++ Design Patterns/DesignPatterns-tentamen/Opdracht3/StemSysteem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht3
+{
+    public class StemSysteem
+    {
+        private static StemSysteem instance;
+
+        private List<string> tegenStemmers = new List<string>();
+        private List<string> voorStemmers = new List<string>();
+
+        public void Stem(string stemCode, bool voorStemmer)
+        {
+            if (!StemCodeGebruikt(stemCode))
+            {
+                if (voorStemmer)
+                {
+                    voorStemmers.Add(stemCode);
+                    Console.WriteLine(stemCode + " is toegevoegd aan de voorstemmers");
+                }
+                else
+                {
+                    tegenStemmers.Add(stemCode);
+                    Console.WriteLine(stemCode + " is toegevoegd aan de tegenstemmers");
+                }
+            }
+            else
+                Console.WriteLine(stemCode + " is al gebruikt");
+        }
+
+        public bool StemCodeGebruikt(string stemCode)
+        {
+            if (tegenStemmers.Contains(stemCode) || voorStemmers.Contains(stemCode))
+                return true;
+
+            return false;
+        }
+
+        public static StemSysteem GetInstance()
+        {
+            if (instance == null)
+                instance = new StemSysteem();
+
+            return instance;
+        }
+    }
+}
